02c4df5761b768897202b4b678992271ee40f2ee
EsperAdmin <dedicated@internetstuff.xyz>
2025-03-26 19:48:01 -0700

    Kinda working?

--- javascript.js
+++ javascript.js
@@ -1,14 +1,22 @@
 //Create function to get a ranondom choice
 function getComputerChoice() {
 //Create 3 variables for rock, paper, and scissors === 0,1, and 2.
-let rock = 1;
-let paper = 2;
-let scissors = 3;
+let rock = 0;
+let paper = 1;
+let scissors = 2;
 //Use math.random to pick a number between 0 and 2
-const getRandom = Math.random(3);
-return console.log(getRandom);
-//create and if else chain to check if number is === to 1,2, or 3
-//return the true response.
+const getRandom = Math.floor(Math.random() * 3);
+//create and else if chain to check if number is === to 1,2, or 3
+if (getRandom === rock) {
+    return console.log("ROCK!");
+} else if (getRandom === paper) {
+    return console.log("PAPER!");
+} else if (getRandom === scissors){
+    return console.log("SCISSORS!");
+} else {
+    return console.log("Uhhh... something went wrong.");
+}
+//return the true response from the else if chain.
 }
 
-console.log(Math.floor(Math.random() * 3));
+console.log(getComputerChoice());
