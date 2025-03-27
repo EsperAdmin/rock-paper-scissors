4a187d935e2aa57c78a665e2ccdd502c1f0aff09
EsperAdmin <dedicated@internetstuff.xyz>
2025-03-26 19:40:39 -0700

    I forgot to commit

--- javascript.js
+++ javascript.js
@@ -1,4 +1,14 @@
 //Create function to get a ranondom choice
 function getComputerChoice() {
-
+//Create 3 variables for rock, paper, and scissors === 0,1, and 2.
+let rock = 1;
+let paper = 2;
+let scissors = 3;
+//Use math.random to pick a number between 0 and 2
+const getRandom = Math.random(3);
+return console.log(getRandom);
+//create and if else chain to check if number is === to 1,2, or 3
+//return the true response.
 }
+
+console.log(Math.floor(Math.random() * 3));
