9f80a30c80bed4188fb26ecf394bc3d242cfef77
EsperAdmin <dedicated@internetstuff.xyz>
2025-03-27 21:23:52 -0700

    create the elese if chain declairing winner and adding points

--- javascript.js
+++ javascript.js
@@ -36,17 +36,17 @@ console.log(`Computer: ${computerChoice} Human: ${humanChoice}`);
 if (humanChoice === computerChoice) {
     return 1;
 } else if (humanChoice === 0 && computerChoice === 1) {
-    return 2;
+    return 21;
 } else if (humanChoice === 1 && computerChoice === 2) {
-    return 2;
+    return 22;
 } else if (humanChoice === 2 && computerChoice === 0) {
-    return 2;
+    return 20;
 } else if (humanChoice === 2 && computerChoice === 1) {
-    return 3;
+    return 31;
 } else if (humanChoice === 0 && computerChoice === 2) {
-    return 3;
+    return 32;
 } else if (humanChoice === 1 && computerChoice === 0) {
-    return 3;
+    return 30;
 }
 
 }
@@ -55,4 +55,36 @@ if (humanChoice === computerChoice) {
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+//Variable to hold round results from playRound function
+let roundResults = playRound(humanSelection, computerSelection);
+
+
+//Else if chain to manage Winner/Loser output and adding points.
+if (roundResults === 1) {
+    console.log("IT'S A DRAW!")
+    console.log(`Human Score: ${humanScore}! Computer Score: ${computerScore}!`)
+} else if (roundResults === 21) {
+    computerScore = ++computerScore;
+    console.log("COMPUTER WINS! PAPER BEATS ROCK!")
+    console.log(`Human Score: ${humanScore}! Computer Score: ${computerScore}!`)
+} else if (roundResults === 22) {
+    computerScore = ++computerScore;
+    console.log("COMPUTER WINS! SCISSORS BEATS PAPER!")
+    console.log(`Human Score: ${humanScore}! Computer Score: ${computerScore}!`)
+} else if (roundResults === 20) {
+    computerScore = ++computerScore;
+    console.log("COMPUTER WINS! ROCK BEATS SCISSORS!")
+    console.log(`Human Score: ${humanScore}! Computer Score: ${computerScore}!`)
+} else if (roundResults === 31) {
+    humanScore = ++humanScore;
+    console.log("HUMAN WINS! SCISSORS BEATS PAPER!")
+    console.log(`Human Score: ${humanScore}! Computer Score: ${computerScore}!`)
+} else if (roundResults === 32) {
+    humanScore = ++humanScore;
+    console.log("HUMAN WINS! ROCK BEATS SCISSORS!")
+    console.log(`Human Score: ${humanScore}! Computer Score: ${computerScore}!`)
+} else if (roundResults === 30) {
+    humanScore = ++humanScore;
+    console.log("HUMAN WINS! PAPER BEATS ROCK!")
+    console.log(`Human Score: ${humanScore}! Computer Score: ${computerScore}!`)
+}
